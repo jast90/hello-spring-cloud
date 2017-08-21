12167dda4b60aaf8d6e80dcbb47d68106d2798e1
zhangzhwien <zhangzhiwen@ubox.cn>
2017-08-21 16:07:19 +0800

    熔断器 服务调用失败调用回调方法

--- hello-consumer/src/main/java/me/jastz/helloconsumer/service/HelloService.java
+++ hello-consumer/src/main/java/me/jastz/helloconsumer/service/HelloService.java
@@ -1,5 +1,6 @@
 package me.jastz.helloconsumer.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,7 +14,12 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "helloFallback")
     public String hello() {
         return restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+    }
+
+    public String helloFallback() {
+        return "error";
     }
 }
