5e6cf405f1416ffdd1462a51a4dc89f7eec04c2a
zhangzhwien <zhangzhiwen@ubox.cn>
2017-08-24 14:04:37 +0800

    add config client

--- hello-consumer/src/main/java/me/jastz/helloconsumer/controller/HelloController.java
+++ hello-consumer/src/main/java/me/jastz/helloconsumer/controller/HelloController.java
@@ -2,9 +2,10 @@ package me.jastz.helloconsumer.controller;
 
 import me.jastz.helloconsumer.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by zhiwen on 2017/8/21.
@@ -15,9 +16,15 @@ public class HelloController {
     @Autowired
     HelloService helloService;
 
+    @Autowired
+    private Environment environment;
+
+    @Value("${suffix:default}")
+    private String suffix;
+
     @RequestMapping("hello")
     public String hello() {
-        return helloService.hello();
+        return helloService.hello() + " " + suffix + " " + environment.getProperty("suffix");
     }
 
 }
